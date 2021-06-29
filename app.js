9552c21451d1548f372e67a76f5db2745f6d8f80
YaroslavBorshevsky812 <Speransky812@mail.ru>
2021-06-29 22:57:48 +0300

    burger button

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const swiper = new Swiper('.section-hero-image', {
-    // // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
-  
-    // // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true
     },
-  
-    // // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-  
-    // // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
   });
+
+document.querySelector('.btn-burger').addEventListener('click', event => {
+  document.querySelector('.section-header').classList.toggle('section-header--active')
+})
