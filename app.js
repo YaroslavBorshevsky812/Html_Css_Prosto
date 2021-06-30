24d1507f3751190b2d5232802aae0b4560d0df85
YaroslavBorshevsky812 <Speransky812@mail.ru>
2021-06-30 21:18:27 +0300

    accordeon

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// === Main Slider === //
+
 const swiper = new Swiper('.section-hero-image', {
     pagination: {
       el: '.swiper-pagination',
@@ -5,6 +7,27 @@ const swiper = new Swiper('.section-hero-image', {
     },
   });
 
+// === // Main Slider === //
+
+// === Burger === //
+
 document.querySelector('.btn-burger').addEventListener('click', event => {
   document.querySelector('.section-header').classList.toggle('section-header--active')
 })
+
+// === // Burger === //
+
+// === Accordeon === //
+
+const btnSel = document.querySelectorAll(".accordeon_item-title")
+
+btnSel.forEach(item => {
+  item.addEventListener('click', event => {
+    if(event.target.closest('.accordeon_item')) {
+          event.target.closest('.accordeon_item')
+          .classList.toggle('active');
+          }
+  })
+})
+
+// === // Accordeon === //
